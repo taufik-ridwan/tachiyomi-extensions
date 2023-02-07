5da129d5414fbd7af06ce400f56ba05d228826ff
Taufik R <75967358+taufik-ridwan@users.noreply.github.com>
2023-02-07 17:22:27 +0700

    Update MangaLife.kt

--- multisrc/overrides/nepnep/mangalife/src/MangaLife.kt
+++ multisrc/overrides/nepnep/mangalife/src/MangaLife.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 class MangaLife : NepNep("MangaLife", "https://manga4life.com", "en") {
 
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
-        .rateLimit(1, 2)
+        .rateLimit(2)
         .connectTimeout(1, TimeUnit.MINUTES)
         .readTimeout(1, TimeUnit.MINUTES)
         .writeTimeout(1, TimeUnit.MINUTES)
